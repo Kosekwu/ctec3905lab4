478e73b664e101e81869d775c9f353ac4e0c50c8
Kose <cosykose@hotmail.co.uk>
2018-11-06 16:50:23 +0000

    adding lab 4 to repos
    adding lab 4 responsive menu, HTML, CSS and JS to repos

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 console.log("hello");
+const menuToggle = document.getElementById('menu-toggle');
+const menuNav = document.getElementById('menu-nav');
+
+function toggleMenu(){
+  console.log("called toggleMenu");
+}
+menuToggle.addEventListener("click", toggleMenu);
